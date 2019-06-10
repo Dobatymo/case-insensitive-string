90ab3f8867274b4420b12db63a87392e67a0d108
Dobatymo <Dobatymo@users.noreply.github.com>
2019-06-10 14:12:15 +0800

    specified arguments
    added cythonized files

--- cis.py
+++ cis.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-# from typing import Tuple, Any
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+	from typing import Tuple, Any
 
 class CaseInsensitiveString(object):
 	"""Case insensitive string """
@@ -24,118 +27,169 @@ class CaseInsensitiveString(object):
 
 	def __ne__(self, other):
 		# type: (Any, ) -> bool
-		return self._s.lower() != other._s.lower()
+
+		if isinstance(other, CaseInsensitiveString):
+			return self._s.lower() != other._s.lower()
+		else:
+			return self._s.lower() != other.lower()
 
 	def __lt__(self, other):
 		# type: (Any, ) -> bool
-		return self._s.lower() < other._s.lower()
+
+		if isinstance(other, CaseInsensitiveString):
+			return self._s.lower() < other._s.lower()
+		else:
+			return self._s.lower() < other.lower()
 
 	def __le__(self, other):
 		# type: (Any, ) -> bool
-		return self._s.lower() <= other._s.lower()
+
+		if isinstance(other, CaseInsensitiveString):
+			return self._s.lower() <= other._s.lower()
+		else:
+			return self._s.lower() <= other.lower()
 
 	def __gt__(self, other):
 		# type: (Any, ) -> bool
-		return self._s.lower() > other._s.lower()
+
+		if isinstance(other, CaseInsensitiveString):
+			return self._s.lower() > other._s.lower()
+		else:
+			return self._s.lower() > other.lower()
 
 	def __ge__(self, other):
 		# type: (Any, ) -> bool
-		return self._s.lower() >= other._s.lower()
+
+		if isinstance(other, CaseInsensitiveString):
+			return self._s.lower() >= other._s.lower()
+		else:
+			return self._s.lower() >= other.lower()
 
 	def __add__(self, other):
 		# type: (CIS, ) -> CIS
-		return CaseInsensitiveString(self._s + other._s)
+
+		if isinstance(other, CaseInsensitiveString):
+			return CaseInsensitiveString(self._s + other._s)
+		else:
+			return CaseInsensitiveString(self._s + other)
 
 	def __mul__(self, other):
 		# type: (int, ) -> CIS
+
 		return CaseInsensitiveString(self._s * other)
 
 	def __mod__(self, other):
 		# type: (Tuple, ) -> CIS
+
 		return CaseInsensitiveString(self._s % other)
 
 	def __len__(self):
-		return len(self._s)
+		return len(self._s) # the length of the lowercase version might be different
 
 	def __iter__(self):
 		# type: () -> Interable[CIS]
+
 		for c in self._s:
 			yield CaseInsensitiveString(c)
 
 	def __hash__(self):
+		# type: () -> int
+
 		return hash(self._s.lower())
 
 	def __getitem__(self, key): # key can be slice object also
 		# type: (int, ) -> CIS
+
 		return CaseInsensitiveString(self._s[key])
 
 	def __contains__(self, other):
 		# type: (Union[str, CIS], ) -> bool
+
 		if isinstance(other, CaseInsensitiveString):
 			return other._s.lower() in self._s.lower()
 		else:
 			return other.lower() in self._s.lower()
 
 	def capitalize(self):
+		# type: () -> CIS
+
 		return CaseInsensitiveString(self._s.capitalize())
 
 	def casefold(self):
+		# type: () -> CIS
+
 		return CaseInsensitiveString(self._s.casefold())
 
-	def center(self, *args, **kwargs):
-		return CaseInsensitiveString(self._s.center(*args, **kwargs))
+	def center(self, width, fillchar=" "):
+		# type: (int, str) -> CIS
 
-	def count(self, sub, *args, **kwargs):
+		return CaseInsensitiveString(self._s.center(width, fillchar))
+
+	def count(self, sub, start=None, end=None):
 		# type: (Union[str, CIS], ) -> int
+
 		if isinstance(sub, CaseInsensitiveString):
-			return self._s.lower().count(sub._s.lower(), *args, **kwargs)
+			return self._s.lower().count(sub._s.lower(), start, end)
 		else:
-			return self._s.lower().count(sub.lower(), *args, **kwargs)
+			return self._s.lower().count(sub.lower(), start, end)
 
-	def encode(self, *args, **kwargs):
-		# type: () -> bytes
-		return self._s.encode(*args, **kwargs)
+	def encode(self, encoding="utf-8", errors="strict"):
+		# type: (str, str) -> bytes
 
-	def endswith(self, suffix, *args, **kwargs):
+		return self._s.encode(encoding, errors)
+
+	def endswith(self, suffix, start=None, end=None):
 		# type: (Union[str, CIS], ) -> bool
+
 		if isinstance(suffix, CaseInsensitiveString):
-			return self._s.lower().endswith(suffix._s.lower(), *args, **kwargs)
+			return self._s.lower().endswith(suffix._s.lower(), start, end)
 		else:
-			return self._s.lower().endswith(suffix.lower(), *args, **kwargs)
+			return self._s.lower().endswith(suffix.lower(), start, end)
 
-	def expandtabs(*args, **kwargs):
-		return CaseInsensitiveString(self._s.expandtabs(*args, **kwargs))
+	def expandtabs(self, tabsize=8):
+		# type: (int, ) -> CIS
 
-	def find(self, sub, *args, **kwargs):
+		return CaseInsensitiveString(self._s.expandtabs(tabsize))
+
+	def find(self, sub, start=None, end=None):
 		# type: (Union[str, CIS], ) -> int
+
 		if isinstance(sub, CaseInsensitiveString):
-			return self._s.lower().find(sub._s.lower(), *args, **kwargs)
+			return self._s.lower().find(sub._s.lower(), start, end)
 		else:
-			return self._s.lower().find(sub.lower(), *args, **kwargs)
+			return self._s.lower().find(sub.lower(), start, end)
 
 	def format(self, *args, **kwargs):
+		# type: (*Any, **Any) -> CIS
+
 		return CaseInsensitiveString(self._s.format(*args, **kwargs))
 
 	def format_map(self, mapping):
+		# type: (Mapping, ) -> CIS
+
 		return CaseInsensitiveString(self._s.format_map(mapping))
 
-	def index(self, sub, *args, **kwargs):
+	def index(self, sub, start=None, end=None):
 		# type: (Union[str, CIS], ) -> int
+
 		if isinstance(sub, CaseInsensitiveString):
-			return self._s.lower().index(sub._s.lower(), *args, **kwargs)
+			return self._s.lower().index(sub._s.lower(), start, end)
 		else:
-			return self._s.lower().index(sub.lower(), *args, **kwargs)
+			return self._s.lower().index(sub.lower(), start, end)
 
 	def isalnum(self):
 		# type: () -> bool
+
 		return self._s.isalnum()
 
 	def isalpha(self):
 		# type: () -> bool
+
 		return self._s.isalpha()
 
 	def isdecimal(self):
 		# type: () -> bool
+
 		return self._s.isdecimal()
 
 	def isdigit(self):
@@ -144,42 +198,57 @@ class CaseInsensitiveString(object):
 
 	def isidentifier(self):
 		# type: () -> bool
+
 		return self._s.isidentifier()
 
 	def islower(self):
 		# type: () -> bool
+
 		return self._s.islower()
 
 	def isnumeric(self):
 		# type: () -> bool
+
 		return self._s.isnumeric()
 
 	def isprintable(self):
 		# type: () -> bool
+
 		return self._s.isprintable()
 
 	def isspace(self):
 		# type: () -> bool
+
 		return self._s.isspace()
 
 	def istitle(self):
 		# type: () -> bool
+
 		return self._s.istitle()
 
 	def isupper(self):
 		# type: () -> bool
+
 		return self._s.isupper()
 
 	def join(self, iterable):
+		# type: (Iterable, ) -> CIS
+
 		return CaseInsensitiveString(self._s.join(iterable))
 
-	def ljust(self, width, *args, **kwargs):
-		return CaseInsensitiveString(self._s.ljust(width, *args, **kwargs))
+	def ljust(self, width, fillchar=" "):
+		# type: (int, str) -> CIS
+
+		return CaseInsensitiveString(self._s.ljust(width, fillchar))
 
 	def lower(self):
+		# type: () -> CIS
+
 		return CaseInsensitiveString(self._s.lower())
 
 	def lstrip(self, chars=None):
+		# type: (CIS, ) -> CIS
+
 		if chars is None:
 			return CaseInsensitiveString(self._s.lstrip()) # only strips case insensitive whitespace anyway
 
@@ -198,12 +267,13 @@ class CaseInsensitiveString(object):
 		return self[i:]
 
 	@staticmethod
-	def maketrans(*args, **kwargs): # create case insensitive dict here?
+	def maketrans(x, y=None, z=None): # create case insensitive dict here?
 		# type: () -> dict
-		return self._s.maketrans(*args, **kwargs)
+		return str.maketrans(x, y, z)
 
 	def partition(self, sep):
 		# type: (Union[str, CIS], ) -> Tuple
+
 		pos = self.find(sep)
 		if pos == -1:
 			return (self, CaseInsensitiveString(""), CaseInsensitiveString("")) # self should be immutable, make copy?
@@ -211,6 +281,8 @@ class CaseInsensitiveString(object):
 			return (self[0:pos], sep, self[pos+len(sep):]) # CaseInsensitiveString(sep) ?
 
 	def replace(self, old, new, count=-1):
+		# type: (str, str, int) -> CIS
+
 
 		if not old and not new:
 			return self
@@ -236,20 +308,24 @@ class CaseInsensitiveString(object):
 
 		return CaseInsensitiveString(ret)
 
-	def rfind(self, sub, *args, **kwargs):
+	def rfind(self, sub, start=None, end=None):
 		# type: (Union[str, CIS], ) -> int
-		return self._s.lower().rfind(sub.lower(), *args, **kwargs) # isinstance separation?
 
-	def rindex(self, sub, *args, **kwargs):
+		return self._s.lower().rfind(sub.lower(), start, end) # isinstance separation?
+
+	def rindex(self, sub, start=None, end=None):
 		# type: (Union[str, CIS], ) -> int
-		return self._s.lower().rindex(sub.lower(), *args, **kwargs) # isinstance separation?
 
-	def rjust(self, width, *args, **kwargs):
-		# type: (int, ) -> CIS
-		return CaseInsensitiveString(self._s.rjust(width, *args, **kwargs))
+		return self._s.lower().rindex(sub.lower(), start, end) # isinstance separation?
+
+	def rjust(self, width, fillchar=" "):
+		# type: (int, str) -> CIS
+
+		return CaseInsensitiveString(self._s.rjust(width, fillchar))
 
 	def rpartition(self, sep):
 		# type: (Union[str, CIS], ) -> Tuple
+
 		pos = self.rfind(sep)
 		if pos == -1:
 			return (CaseInsensitiveString(""), CaseInsensitiveString(""), self) # self should be immutable
@@ -258,10 +334,11 @@ class CaseInsensitiveString(object):
 
 	def rsplit(self, sep=None, maxsplit=-1):
 		# type: (Union[str, CIS], int) -> CIS
+
 		if sep is None:
 			return list(CaseInsensitiveString(s) for s in self._s.rsplit(sep, maxsplit))
 		else:
-			raise NeedsOwnImplementation
+			raise NotImplementedError
 
 	def rstrip(self, chars=None):
 		if chars is None:
@@ -305,16 +382,17 @@ class CaseInsensitiveString(object):
 			ret.append(CaseInsensitiveString(self._s[pos:]))
 			return ret
 
-	def splitlines(self, *args, **kwargs):
-		# type: () -> list
-		return list(CaseInsensitiveString(line) for line in self._s.splitlines(*args, **kwargs))
+	def splitlines(self, keepends=False):
+		# type: (bool, ) -> list
+		return list(CaseInsensitiveString(line) for line in self._s.splitlines(keepends))
 
-	def startswith(self, prefix, *args, **kwargs):
+	def startswith(self, prefix, start=None, end=None):
 		# type: (Union[str, CIS], ) -> bool
+
 		if isinstance(prefix, CaseInsensitiveString):
-			return self._s.lower().startswith(prefix._s.lower(), *args, **kwargs)
+			return self._s.lower().startswith(prefix._s.lower(), start, end)
 		else:
-			return self._s.lower().startswith(prefix.lower(), *args, **kwargs)
+			return self._s.lower().startswith(prefix.lower(), start, end)
 
 	def strip(self, chars=None):
 		if chars is None:
@@ -327,18 +405,26 @@ class CaseInsensitiveString(object):
 			return CaseInsensitiveString(self._s.strip(chars))
 
 	def swapcase(self):
+		# type: () -> CIS
+
 		return CaseInsensitiveString(self._s.swapcase())
 
 	def title(self):
+		# type: () -> CIS
+
 		return CaseInsensitiveString(self._s.title())
 
 	def translate(self, table):
 		return CaseInsensitiveString(self._s.translate(table))
 
 	def upper(self):
+		# type: () -> CIS
+
 		return CaseInsensitiveString(self._s.upper())
 
 	def zfill(self, width):
+		# type: (int, ) -> CIS
+
 		return CaseInsensitiveString(self._s.zfill(width))
 
 class StringEx(CaseInsensitiveString):
@@ -350,7 +436,7 @@ class StringEx(CaseInsensitiveString):
 
 CIS = CaseInsensitiveString
 
-from utils_test import MyTestCase, parametrize
+from ferutility.test import MyTestCase, parametrize
 class CaseInsensitiveStringTest(MyTestCase):
 
 	@parametrize(
--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-	ext_modules = cythonize("cis.pyx")
+	name="case-insensitive-string",
+	version="0.0.1",
+	ext_modules=cythonize(["cis.py", "ccis.pyx"])
 )
